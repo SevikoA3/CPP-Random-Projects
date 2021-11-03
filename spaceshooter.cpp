bb1d2bcb212135359864395021573b2960782217
SevikoA3 <attalarik.handoko@gmail.com>
2021-11-03 08:40:13 +0700

    adding highscore and option to play again

--- spaceshooter.cpp
+++ spaceshooter.cpp
@@ -42,15 +42,11 @@ void Setup(){
 
 void Logic(){
 	if (!bulletX.empty()){
-		for (int i = 0; i < bulletX.size(); i++){
-			bulletY[i]++;
-		}
+		for (int i = 0; i < bulletX.size(); i++) bulletY[i]++;
 	}
 	meteorT++;
 	level++;
-	if (level % 100 == 0 && mod > 0){
-		mod--;
-	}
+	if (level % 100 == 0 && mod > 0) mod--;
 	if (meteorT % mod == 0){
 		meteorX.push_back((rand() % (height-4) + 2));
 		meteorY.push_back(width-2);
